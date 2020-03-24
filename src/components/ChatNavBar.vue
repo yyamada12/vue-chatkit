ae47f3b688478d68099bc36aae8d39ed09aa4130
yyamada12 <12yacropolisy@gmail.com>
2020-03-25 08:57:39 +0900

    reconnect user after page refresh

--- src/components/ChatNavBar.vue
+++ src/components/ChatNavBar.vue
@@ -9,11 +9,31 @@
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapState, mapActions, mapMutations } from "vuex";
 export default {
   name: "ChatNavBar",
   computed: {
-    ...mapState(["user"])
+    ...mapState(["user", "reconnect"])
+  },
+  methods: {
+    ...mapActions(["logout", "login"]),
+    ...mapMutations(["setReconnect"]),
+    onLogout() {
+      this.$router.push({ path: "/" });
+      this.logout();
+    },
+    unload() {
+      //User hasn't logged out
+      if (this.user.username) {
+        this.setReconnect(true);
+      }
+    }
+  },
+  mounted() {
+    window.addEventListener("beforeunload", this.unload);
+    if (this.reconnect) {
+      this.login(this.user.username);
+    }
   }
 };
 </script>
